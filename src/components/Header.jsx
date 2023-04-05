efd236931db00b5982e6884bc28145af6b4ca1ac
connectnitesh <connectnitesh8@gmail.com>
2023-04-06 01:57:51 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EditNoteIcon from '@mui/icons-material/EditNote';
+
+function Header() {
+  return (
+    <header>
+      <h1><EditNoteIcon />Note Making</h1>
+    </header>
+  );
+}
+
+export default Header;
