4a9eb562ab185ac6f252796cfb03c142d22c8ada
connectnitesh <connectnitesh8@gmail.com>
2023-06-24 10:56:02 +0530

    update 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import NoteIcon from './image.png';
+
+
+function Header() {
+
+  return (
+    <header>
+      <img src={NoteIcon} alt="" />
+      <h1>Note Keep</h1>
+      
+    </header>
+  );
+}
+
+export default Header;
